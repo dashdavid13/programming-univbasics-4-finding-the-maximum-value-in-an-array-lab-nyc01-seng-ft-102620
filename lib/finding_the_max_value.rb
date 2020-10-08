d11f71cfc41ed334ffc58280a831692d923d2797
David Medina <davidamedina619@gmail.com>
2020-10-08 18:25:12 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,15 +1,3 @@
-def find_max_value(array)
-  count = 0
-  max_value = -1
-  
-  while count < array.length do
-    if max_value < array[count]
-      max_value = array[count]
-    end 
-    count +=1
-  end 
-  max_value
-end
 
 
  #def find_element_index(array, value_to_find)
@@ -22,3 +10,13 @@ end
  #end 
   # found_value_index
  #end
+
+
+def  find_max_value(array)
+  max_value = -1
+  array.each do  |index|
+    if max_value < index
+      max_value = index
+  end 
+  max_value
+end
